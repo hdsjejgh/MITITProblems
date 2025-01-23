f62598f48a8990a31a8ffadd0d67e95d05cc0b10
hdsjejgh <140638415+hdsjejgh@users.noreply.github.com>
2025-01-23 18:09:42 -0500

    Delete MITIT.py

--- MITIT.py
+++
@@ -1,9 +0,0 @@
-def solve(string):
-    for idx in range(1,len(string)):
-        if len(string[idx:])%2!=0:
-            continue
-        if string[idx:int(idx+len(string[idx:])/2)]==string[int(idx+len(string[idx:])/2):]:
-            return 'YES'
-    return 'NO'
-for i in range(int(input())):
-    print(solve(input()))
